4436f9287c6e139f82696706b03948696378a4e2
Michael <loukerismichalis@gmail.com>
2018-11-02 18:51:01 +0200

    zram: Use lz4 compression by default
    Signed-off-by: starlight5234 <ifist2834@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -46,6 +46,12 @@ static const char *default_compressor = "lz4";
 static const char *default_compressor = "lzo";
 #endif
 
+/*
+ * We don't need to see memory allocation errors more than once every 1
+ * second to know that a problem is occurring.
+ */
+#define ALLOC_ERROR_LOG_RATE_MS 1000
+
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
 /*
