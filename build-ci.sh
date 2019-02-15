b23eb42a0ab66331d577ba09f0141fab1bda1c13
urK <38219997+Z5X67280@users.noreply.github.com>
2019-02-15 21:36:21 +0800

    Update build-ci.sh

--- build-ci.sh
+++ build-ci.sh
@@ -10,11 +10,12 @@ export KBUILD_BUILD_HOST="-buildaesthesia- Travis-CI"
 
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 Toolchain --depth=1
 
-make O=out vince-perf_defconfig -j64
-make O=out -j64
+make O=out vince-perf_defconfig -j96
+make O=out -j96
 
-cp out/arch/arm64/boot/Image.gz nito-ak2/kernel
-cp out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-vince.dtb nito-ak2/kernel/treble
-zip "Nito Kernel CI.zip" nito-ak2/
+cp out/arch/arm64/boot/Image.gz nito-ak2/kernel/
+cp out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-vince.dtb nito-ak2/kernel/treble/
+cd nito-ak2/
+zip "Nito Kernel CI.zip" *
 
 echo "Build done!"
