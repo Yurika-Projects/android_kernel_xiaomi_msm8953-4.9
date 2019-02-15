42e455de800d6bee980c3855b60aa4967d1f2d67
urK -kernelaesthesia <Z5X67280@163.com>
2019-02-15 23:02:47 +0800

    build-ci.sh: Add telegram channel support
    Signed-off-by: urK -kernelaesthesia <Z5X67280@163.com>

--- build-ci.sh
+++ build-ci.sh
@@ -1,6 +1,76 @@
 # Nito CI Script
 # Copyright (C) 2019 urK -kernelaesthesia- (Z5X67280@163.com)
+# Copyright (C) 2019 Raphiel Rollerscaperers (raphielscape)
+# Copyright (C) 2019 Rama Bondan Prakoso (rama982) 
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+#
+# Telegram FUNCTION begin
+#
+
+git clone https://github.com/fabianonline/telegram.sh telegram
+
+TELEGRAM_ID=-1001346873717
+TELEGRAM=telegram/telegram
+TELEGRAM_TOKEN=${BOT_API_KEY}
+
+export TELEGRAM_TOKEN
+
+# Push kernel installer to channel
+function push() {
+	JIP="Nito Kernel CI.zip"
+	curl -F document=@$JIP  "https://api.telegram.org/bot$BOT_API_KEY/sendDocument" \
+			-F chat_id="$TELEGRAM_ID"
+}
+
+# Send the info up
+function tg_channelcast() {
+	"${TELEGRAM}" -c ${TELEGRAM_ID} -H \
+		"$(
+			for POST in "${@}"; do
+				echo "${POST}"
+			done
+		)"
+}
+
+function tg_sendinfo() {
+	curl -s "https://api.telegram.org/bot$BOT_API_KEY/sendMessage" \
+		-d "parse_mode=markdown" \
+		-d text="${1}" \
+		-d chat_id="$TELEGRAM_ID" \
+		-d "disable_web_page_preview=true"
+}
+
+# Errored prober
+function finerr() {
+	tg_sendinfo "$(echo -e "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds\nbut it's error...")"
+	exit 1
+}
+
+# Send sticker
+function tg_sendstick() {
+	curl -s -X POST "https://api.telegram.org/bot$BOT_API_KEY/sendSticker" \
+		-d sticker="CAADAQADDgEAAtYvmwa3XVlL__xsuwI" \
+		-d chat_id="$TELEGRAM_ID" >> /dev/null
+}
+
+# Fin prober
+function fin() {
+	tg_sendinfo "$(echo "Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.")"
+}
+
+#
+# Telegram FUNCTION end
+#
+
+tg_sendstick
+
+tg_channelcast "<b>Nito Kernel</b> new build!" \
+		"Started on <code>$(hostname)</code>" \
+		"For device </b>Redmi 5 Plus</b>" \
+		"At branch <code>9.0-caf-upstream</code>" \
+		"Under commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>" \
+		"Started on <code>$(date)</code>"
 
 export ARCH=arm64
 export SUBARCH=arm64
@@ -19,5 +89,8 @@ cp out/arch/arm64/boot/Image.gz nito-ak2/kernel/
 cp out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-vince.dtb nito-ak2/kernel/treble/
 cd nito-ak2/
 zip "Nito Kernel CI.zip" *
+push
+cd ..
 
+fin
 echo "Build done!"
