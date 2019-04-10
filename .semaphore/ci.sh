22bde47ae5661001206eccfe13a323ef9e339a85
urK <38219997+Z5X67280@users.noreply.github.com>
2019-04-10 18:38:03 +0800

    CI: Nito Kernel CI Script v2.1
    - Improve Build Perf
    - Del unused env
    - Remove unused stuff
    - Add ccache support
    - Set Clang compiler clone branch

--- .semaphore/ci.sh
+++ .semaphore/ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Nito CI Script v2
+# Nito CI Script v2.1
 # Copyright (C) 2019 urK -kernelaesthesia- (Z5X67280@163.com)
 # Copyright (C) 2019 Raphiel Rollerscaperers (raphielscape)
 # Copyright (C) 2019 Rama Bondan Prakoso (rama982) 
@@ -15,9 +15,6 @@ git clone https://github.com/fabianonline/telegram.sh telegram
 TELEGRAM_ID=-1001268516549
 TELEGRAM=telegram/telegram
 BOT_API_KEY=723044228:AAFpmF9aHsMTinCJ7Yq3HLxEzjEBiO47rlU
-TELEGRAM_TOKEN=${BOT_API_KEY}
-
-export TELEGRAM_TOKEN
 
 # Push kernel installer to channel
 function push() {
@@ -65,13 +62,6 @@ function finerr() {
 	exit 1
 }
 
-# Send Release Photo
-function r6relphotosend() {
-	curl -s "https://api.telegram.org/bot$BOT_API_KEY/sendPhoto" \
-	     -d chat_id="$TELEGRAM_ID" \
-	     -d photo="../rel.jpg"
-}
-
 #
 # Telegram FUNCTION end
 #
@@ -96,16 +86,18 @@ tg_sendstick
 
 tg_channelcast "<b>Nito Kernel</b> new build!" \
 		"Version: <b>$VERSION_TG</b>" \
-		"Started on <b>Ubuntu 16.04 LTS (Xenial)</b>" \
+		"Started on <b>Ubuntu 18.04 LTS (Bionic)</b>" \
 		"From <b>9.0-caf-upstream (Nito Kernel Mainline) </b>" \
 		"Under commit <b>$(git log --pretty=format:'"%h : %s"' -1)</b>" \
 		"Started on <b>$(date)</b>"
 
 git clone https://github.com/krasCGQ/aarch64-linux-android -b opt-gnu-8.x --depth=1 Toolchain
-git clone https://github.com/Z5X67280/aosp-clang-mirror --depth=1 Clang
+git clone https://github.com/Z5X67280/aosp-clang-mirror -b clang-r353983 --depth=1 Clang
 
-make O=out vince-perf_defconfig -j40
-make O=out -j40
+sudo apt install ccache -y
+
+make O=out vince-perf_defconfig -j64
+make O=out -j64
 
 if ! [ -a out/arch/arm64/boot/Image.gz-dtb ]; then
 	echo -e "Kernel compilation failed, See buildlog to fix errors"
@@ -129,7 +121,6 @@ tg_channelcast "Notes: Don't try install it on non-4.9 roms." \
 push
 cd ..
 fin
-r6relphotosend
 
 echo "Build done!"
 
