562388721e35341386f9458027374a1b17f1d0e7
Nathan Chancellor <natechancellor@gmail.com>
2019-07-03 14:16:06 -0700

    Merge tag 'LA.UM.7.6.2.r1-09200-89xx.0' into kernel.lnx.4.9.r12-rel
    "LA.UM.7.6.2.r1-09200-89xx.0"
    
    * tag 'LA.UM.7.6.2.r1-09200-89xx.0':
      msm: ipa3: Fix to validate check for IP type
      diag: Prevent out-of-bound access while processing userspace data
      icnss: block modem shutdown during driver reinit
      msm: adsprpc: Fix integer overflow in refcount of map
      msm: vidc: add additional check to avoid out of bound access
      msm: adsprpc: maintain local copy of rpra offloaded to DSP
      Merge remote-tracking branch 'dev/msm-4.14' into msm-4.9 05/20
      soc: qcom: smem: validate fields of shared structures
      msm: camera: isp: Prevent out of bounds read
      msm: vidc: correct buffer size calcuation logic for output plane
      drm/msm/dsi-staging: Add range check in debugfs_dump_info_read
      qcom: smcinvoke: Fix stack overflow for arr_filp
      drm/msm/sde: stage border fill after ctl start timeout
      msm: ipa: fix to validate input parameters
      diag: Check command size against the minimum before parsing
      power: qpnp-smb5: Handle cc_soc jump in the overcharge WA
      power: qpnp-qg/fg-gen3: Report the REAL_CAPACITY
      power: power_supply: Add REAL_CAPACITY power-supply property
      power: qpnp-fg: Do not return -EAGAIN when handling IMA error during probe
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_encoder.c
+++ drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2019, The Linux Foundation. All rights reserved.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
  *
@@ -3094,19 +3094,22 @@ int sde_encoder_idle_request(struct drm_encoder *drm_enc)
 	return 0;
 }
 
-static void sde_encoder_off_work(struct kthread_work *work)
+int sde_encoder_get_ctlstart_timeout_state(struct drm_encoder *drm_enc)
 {
-	struct sde_encoder_virt *sde_enc = container_of(work,
-			struct sde_encoder_virt, delayed_off_work.work);
-	struct drm_encoder *drm_enc;
+	struct sde_encoder_virt *sde_enc = NULL;
+	int i, count = 0;
 
-	if (!sde_enc) {
-		SDE_ERROR("invalid sde encoder\n");
-		return;
+	if (!drm_enc)
+		return 0;
+
+	sde_enc = to_sde_encoder_virt(drm_enc);
+
+	for (i = 0; i < sde_enc->num_phys_encs; i++) {
+		count += atomic_read(&sde_enc->phys_encs[i]->ctlstart_timeout);
+		atomic_set(&sde_enc->phys_encs[i]->ctlstart_timeout, 0);
 	}
-	drm_enc = &sde_enc->base;
 
-	sde_encoder_idle_request(drm_enc);
+	return count;
 }
 
 /**
