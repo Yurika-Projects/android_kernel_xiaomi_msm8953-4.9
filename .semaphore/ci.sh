22b34e330004cebecf579cdb310c3e66db036b11
Keternal Witch <Z5X67280@163.com>
2019-07-05 21:48:00 +0800

    semaphore: Change Develop Status
    Signed-off-by: Keternal Witch <Z5X67280@163.com>

--- .semaphore/ci.sh
+++ .semaphore/ci.sh
@@ -96,7 +96,7 @@ tg_channelcast "------------------------"
 tg_sendstick
 
 tg_channelcast "<b>Nito Kernel $VERSION_TG</b> new build!" \
-		"Stage: <b>Merge Latest CAF Tag</b>" \
+		"Stage: <b>Do Some Optimize</b>" \
 		"From <b>Nito Kernel Mainline</b>" \
 		"Under commit <b>$(git log --pretty=format:'%h' -1)</b>"
 
