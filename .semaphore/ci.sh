56c110919bf7e85fc35109b4522534e236ea1962
Keternal <Z5X67280@163.com>
2019-08-14 02:39:38 +0800

    ci: set status
    Signed-off-by: Keternal <Z5X67280@163.com>

--- .semaphore/ci.sh
+++ .semaphore/ci.sh
@@ -96,7 +96,7 @@ tg_channelcast "------------------------"
 tg_sendstick
 
 tg_channelcast "<b>Nitro Kernel $VERSION_TG</b> new build!" \
-		"Stage: <b>Goodnight.</b>" \
+		"Stage: <b>Fix Bug</b>" \
 		"From <b>Nitro Kernel Mainline</b>" \
 		"Under commit <b>$(git log --pretty=format:'%h' -1)</b>"
 
