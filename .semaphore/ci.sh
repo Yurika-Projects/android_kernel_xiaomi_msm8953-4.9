2d7ee86afb863de2fea3e2cd0a9d9bcf6e80aae4
Keternal <Z5X67280@163.com>
2019-08-17 16:52:40 +0800

    Anykernel3: Genom-ify
    Signed-off-by: Keternal <Z5X67280@163.com>

--- .semaphore/ci.sh
+++ .semaphore/ci.sh
@@ -121,8 +121,8 @@ export DIFF=$(($BUILD_END - $BUILD_START))
 export BUILD_POINT=$(git log --pretty=format:'%h' -1)
 
 # Pack
-cp $IMG AnyKernel2/
-cd AnyKernel2/
+cp $IMG AnyKernel3/
+cd AnyKernel3/
 zip -r9 -9 "Genom-Kernel-$ZIP_VERSION-$BUILD_TYPE-$BUILD_POINT.zip" .
 md5sum Genom-Kernel-$ZIP_VERSION-$BUILD_TYPE-$BUILD_POINT.zip >> "md5sum_$(git log --pretty=format:'%h' -1).md5sum"
 
