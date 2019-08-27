4b63fc2a584786b446b848f220b94de0fc379783
Keternal <Z5X67280@163.com>
2019-08-27 14:01:13 +0800

    ci: PERFECT
    Signed-off-by: Keternal <Z5X67280@163.com>

--- .semaphore/ci.sh
+++ .semaphore/ci.sh
@@ -87,11 +87,11 @@ export ARCH=arm64
 export SUBARCH=arm64
 export CLANG_TREPLE=aarch64-linux-gnu-
 export CROSS_COMPILE="$PWD/Toolchain/bin/aarch64-opt-linux-android-"
-export KBUILD_BUILD_USER="Falling Star"
+export KBUILD_BUILD_USER="Perfect"
 export KBUILD_BUILD_HOST="TNR Drone"
 export IMG=$PWD/out/arch/arm64/boot/Image.gz-dtb
-export VERSION_TG="rXe Sunset Radience"
-export ZIP_VERSION="rXe"
+export VERSION_TG="rXF Pictured as Perfect"
+export ZIP_VERSION="rXF"
 export BUILD_TYPE="REL"
 
 # Telegram Stuff 
@@ -101,7 +101,7 @@ tg_channelcast "#########################"
 tg_sendstick
 
 tg_channelcast "<b>Nito Kernel $VERSION_TG</b> new build!" \
-		"Stage: <b>Refactor from 3145bec5a94</b>" \
+		"Stage: <b>PERFECT</b>" \
 		"From <b>Nito Kernel Mainline</b>" \
 		"Under commit <b>$(git log --pretty=format:'%h' -1)</b>"
 
