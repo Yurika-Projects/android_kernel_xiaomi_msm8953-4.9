e4a6ec8a0c666fe0c2a45c5dbdf70e6305446722
Keternal <Z5X67280@163.com>
2019-09-30 01:18:54 +0800

    ci: Set status
    Signed-off-by: Keternal <Z5X67280@163.com>

--- .semaphore/ci.sh
+++ .semaphore/ci.sh
@@ -101,7 +101,7 @@ tg_channelcast "#########################"
 tg_sendstick
 
 tg_channelcast "<b>Nito Kernel $VERSION_TG</b> new build!" \
-		"Stage: <b>Checkout Back to CAF initial Energy Model</b>" \
+		"Stage: <b>Tune Energy Model</b>" \
 		"From <b>Nito Kernel Mainline</b>" \
 		"Under commit <b>$(git log --pretty=format:'%h' -1)</b>"
 
