c33ca3b99c7875a3a22b86d4f2973a5337021f17
Keternal <Z5X67280@163.com>
2019-10-20 10:19:03 +0800

    ci: Set status

--- .semaphore/ci.sh
+++ .semaphore/ci.sh
@@ -105,7 +105,7 @@ tg_channelcast "#########################"
 tg_sendstick
 
 tg_channelcast "<b>Nito Kernel $VERSION_TG</b> new build!" \
-		"Stage: <b>Update Drivers</b>" \
+		"Stage: <b>Kernel Optimize</b>" \
 		"From <b>Nito Kernel Mainline</b>" \
 		"Under commit <b>$(git log --pretty=format:'%h' -1)</b>"
 
