be6339ace4b8a143f71bbc338daf5517ec07b6d8
Keternal <Z5X67280@163.com>
2019-10-30 20:10:02 +0800

    ci: Stay Build Status
    Signed-off-by: Keternal <Z5X67280@163.com>

--- .semaphore/ci.sh
+++ .semaphore/ci.sh
@@ -105,7 +105,7 @@ tg_channelcast "#########################"
 tg_sendstick
 
 tg_channelcast "<b>Nito Kernel $VERSION_TG</b> new build!" \
-		"Stage: <b>Initial Build</b>" \
+		"Stage: <b>Build</b>" \
 		"From <b>Nito Kernel Android 10 Mainline</b>" \
 		"Under commit <b>$(git log --pretty=format:'%h' -1)</b>"
 
