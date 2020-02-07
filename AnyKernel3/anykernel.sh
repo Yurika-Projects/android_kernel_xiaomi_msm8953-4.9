44d10d57ae74551aaa19cf7c6557c91ebbd83173
Keternal <Z5X67280@163.com>
2020-02-07 14:17:24 +0800

    AK3: Introduce New Anykernel 3
    - From
      https://github.com/osm0sis/AnyKernel3/commit/dfcfb87e6e88562476c1a5b304385838ff802709
    
    Signed-off-by: Keternal <Z5X67280@163.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -7,25 +7,21 @@ properties() { '
 kernel.string=Nito Kernel Install
 do.devicecheck=1
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
-supported.versions=29
 device.name1=vince
+supported.versions=29
 '; } # end properties
 
 # shell variables
 block=/dev/block/mmcblk0p21;
 ramdisk_compression=auto;
 is_slot_device=0;
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
 ## AnyKernel install
 dump_boot;
-
 write_boot;
-
 ## end install
-
